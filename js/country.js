ed5c69d4459e903fd2cbe29d724e9d7cdbb02fbd
Metinkeles1 <97405379+Metinkeles1@users.noreply.github.com>
2022-07-27 21:19:50 +0300

    update

--- js/country.js
+++ js/country.js
@@ -33,7 +33,8 @@ async function listCountry(region) {
             document.querySelector(".loading-center").style.display = "none";
         }
         const data = await response.json();
-        displayCountry(data);
+        // displayCountry(data)
+        renderCountries(data);
     } catch (error) {
         console.log(error)
     }
@@ -43,71 +44,54 @@ async function listCountry(region) {
 
 getRegion();
 
+function renderCountries(allCountries) {
+    const detailsCountryBtn = document.querySelector('#detailsCountry');
+    detailsCountryBtn.parentElement.style.display = "flex";
+    let firstDisplayCountry = [];
+    let detailsCountry = [];
+    for (let i = 0; i < 12; i++) firstDisplayCountry.push(allCountries[i]);
+    detailsCountryBtn.addEventListener('click', () => {
+        for (let i = 12; i < allCountries.length; i++) detailsCountry.push(allCountries[i]);
+        console.log(detailsCountry)
+        displayCountry(firstDisplayCountry.concat(detailsCountry))
+    })
+    displayCountry(firstDisplayCountry);
+}
+
 
 function displayCountry(allCountries) {
     document.querySelector(".loading-center").style.display = "none";
     let html = "";
 
-    for (let i = 0; i < 12; i++) {
+    for (let country of allCountries) {
         html += `
         <div class="card-country col-3 my-4 mx-4">
-                <img src="${allCountries[i].flags.png}" id="countryImg" class="img-fluid">
+                <img src="${country.flags.png}" id="countryImg" class="img-fluid">
                 <div class="card card-country-body">
                     <div id="countryTitle" class="card-title card-country-header">
-                        <h3 class="my-2">${allCountries[i].name.common}</h3>
+                        <h3 class="my-2">${country.name.common}</h3>
                     </div>
                     <div id="countyrText" class="card-text card-country-text mx-2">
                         <div class="my-2">
                             <div class="d-inline">Başkent: </div>
-                            <div class="d-inline">${allCountries[i].capital}</div>
+                            <div class="d-inline">${country.capital}</div>
                         </div>
                         <div class="my-2">
                             <div class="d-inline">Bölge: </div>
-                            <div class="d-inline">${allCountries[i].region}</div>
+                            <div class="d-inline">${country.region}</div>
                         </div>
                         <div class="my-2">
                             <div class="d-inline">Popülasyon: </div>
-                            <div class="d-inline">${(allCountries[i].population / 1000000).toFixed(1)} milyon</div>
+                            <div class="d-inline">${(country.population / 1000000).toFixed(1)} milyon</div>
                         </div>
                         <div class="my-2">
                             <div class="d-inline">Para Birimi: </div>
-                            <div class="d-inline">${Object.values(allCountries[i].currencies)[0].name} (${Object.values(allCountries[i].currencies)[0].symbol})</div>
+                            <div class="d-inline">${Object.values(country.currencies)[0].name} (${Object.values(country.currencies)[0].symbol})</div>
                         </div>
                     </div>
                 </div>
         </div>
         `;
     }
-
-    // for (let country of allCountries) {
-    //     html += `
-    //     <div class="card-country col-3 my-4 mx-4">
-    //             <img src="${country.flags.png}" id="countryImg" class="img-fluid">
-    //             <div class="card card-country-body">
-    //                 <div id="countryTitle" class="card-title card-country-header">
-    //                     <h3 class="my-2">${country.name.common}</h3>
-    //                 </div>
-    //                 <div id="countyrText" class="card-text card-country-text mx-2">
-    //                     <div class="my-2">
-    //                         <div class="d-inline">Başkent: </div>
-    //                         <div class="d-inline">${country.capital}</div>
-    //                     </div>
-    //                     <div class="my-2">
-    //                         <div class="d-inline">Bölge: </div>
-    //                         <div class="d-inline">${country.region}</div>
-    //                     </div>
-    //                     <div class="my-2">
-    //                         <div class="d-inline">Popülasyon: </div>
-    //                         <div class="d-inline">${(country.population / 1000000).toFixed(1)} milyon</div>
-    //                     </div>
-    //                     <div class="my-2">
-    //                         <div class="d-inline">Para Birimi: </div>
-    //                         <div class="d-inline">${Object.values(country.currencies)[0].name} (${Object.values(country.currencies)[0].symbol})</div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //     </div>
-    //     `;
-    // }
     document.querySelector('#countriesAll').innerHTML = html;
 }
